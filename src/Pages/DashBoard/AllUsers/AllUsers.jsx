5244dc483105d95ed0222e962432e751bb469972
programming hero <naofelbadsha@gmail.com>
2024-09-17 20:19:42 +0100

    added user admin successfull

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -19,10 +19,9 @@ const AllUsers = () => {
   //----------Deleted---------user-------
   const handleDeleteUser = user => {
     console.log("Deleted user..?", user)
-
     Swal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this!",
+      text: `${user.name} is Deleted!`,
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
@@ -37,7 +36,7 @@ const AllUsers = () => {
             refetch()
             Swal.fire({
               title: "Deleted!",
-              text: "Your file has been deleted.",
+              text: "Deleted Successfull.",
               icon: "success"
             });
 
@@ -45,6 +44,24 @@ const AllUsers = () => {
         })
       }
     });
+  }
+
+  //----------Admin---------user-------
+  const handleAdminUser = user => {
+    axiosSecure.patch(`/users/admin/${user._id}`)
+    .then(res => {
+      console.log(res.data)
+      if(res.data.modifiedCount > 0){
+        refetch();
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: `${user.name} is an Admin Now!`,
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
   }
   return (
     <div>
@@ -85,10 +102,10 @@ const AllUsers = () => {
                   <td className="text-xl text-black">{user.name}</td>
                   <td className="text-xl text-black">{user.email}</td>
                   <td>
-                    <button
+                    {user.role === "admin" ? <h3 className="text-xl text-[#005d5d]">Admin</h3> : <button onClick={() => handleAdminUser(user)}
                      className="btn text-3xl text-white bg-[#008080] border-0 ">
                       <FaUsers />
-                    </button>
+                    </button>}
                   </td>
                   <td>
                     <button 
