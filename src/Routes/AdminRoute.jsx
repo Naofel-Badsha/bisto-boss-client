cec5e9d416e0b21536402b7e5b13b2030faf2f96
programming hero <naofelbadsha@gmail.com>
2024-09-18 11:12:35 +0100

    added admin route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,20 +1,20 @@
-
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
 import useAuth from "../Hooks/useAuth";
 
-// import useAuth from "../Hooks/useAuth";
+const AdminRoute = ({children}) => {
+    const [user, loading] = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
 
-
-const PrivateRoute = ({ children }) => {
-  const { user, loading } = useAuth();
-  const location = useLocation();
-  if (loading) {
+    const location = useLocation();
+  if (loading || isAdminLoading) {
     return <div className="flex item-center justify-center"> <img src="https://i.ibb.co.com/6Bt99B3/loding-3.gif" className="w-[200px] " /></div>
   }
-  if (user) {
+  if (user && isAdmin) {
     return children;
   }
   return <Navigate to="/login" state={{from: location}} replace></Navigate>;
+
 };
 
-export default PrivateRoute;
+export default AdminRoute;
