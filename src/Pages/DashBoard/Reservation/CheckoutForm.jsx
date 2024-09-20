a0378d1dae7e5d1a099485d72d560d9ca1fecdff
programming hero <naofelbadsha@gmail.com>
2024-09-20 16:38:20 +0100

    added paymeny method complite

--- src/Pages/DashBoard/Reservation/CheckoutForm.jsx
+++ src/Pages/DashBoard/Reservation/CheckoutForm.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useCart from "../../../Hooks/useCart";
 import useAuth from "../../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 const CheckoutForm = () => {
   const [error, setError] = useState('');
@@ -12,16 +13,18 @@ const CheckoutForm = () => {
   const elements = useElements();
   const axiosSecure = useAxiosSecure();
   const {user} = useAuth();
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0)
 
   //--------client to server post data--------
   useEffect(() => {
-    axiosSecure.post('/create-payment-intent', {price: totalPrice})
-    .then(res =>{
-       console.log(res.data.clientSecret);
-       setClientSecret(res.data.clientSecret) 
-    })
+    if(totalPrice > 0){
+      axiosSecure.post('/create-payment-intent', {price: totalPrice})
+      .then(res =>{
+         console.log(res.data.clientSecret);
+         setClientSecret(res.data.clientSecret) 
+      })
+    }
 },[axiosSecure, totalPrice])
 
   const handleSubmit = async (event) => {
@@ -69,9 +72,32 @@ const CheckoutForm = () => {
         if(paymentIntent.status === "succeeded"){
           console.log('transaction id', paymentIntent.id)
           setTransactionId(paymentIntent.id);
+          //--------Now save the payment in the database-----
+          const payment = {
+            email: user.email,
+            price: totalPrice,
+            transactionId: paymentIntent.id,
+            date: new Date(), //-- utc date convert, use moment js
+            cartIds: cart.map(item => item._id),
+            menuItemIds: cart.map(item => item.menuId),
+            status: 'pending'
+          }
+        const res = await axiosSecure.post('/payments', payment)
+        console.log('payment save',res.data);
+        refetch()
+        if(res.data?.paymentResult?.insertedId){
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Thank you for the payment",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
         }
       }
   };
+
 
   return (
     <form onSubmit={handleSubmit} className="border-2 border-[#008080] p-10">
