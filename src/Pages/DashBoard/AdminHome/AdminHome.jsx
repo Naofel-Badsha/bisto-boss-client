4aae21a8a140deb6e2116120eb9d610f556f7e5c
programming hero <naofelbadsha@gmail.com>
2024-09-21 18:35:50 +0100

    added adminHome all cards

--- src/Pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/Pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,8 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import useAuth from "../../../Hooks/useAuth";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaDollarSign } from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
+import { GiAmpleDress } from "react-icons/gi";
+import { FaFirstOrder } from "react-icons/fa";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+
   return (
     <div>
       {/*----------Title---------*/}
@@ -16,6 +32,50 @@ const AdminHome = () => {
           <span>Hi, WellCome</span>{" "}
           {user?.displayName ? user.displayName : "Back!"}
         </h3>
+      </div>
+
+      {/*------------stat-------------*/}
+      <div className="flex flex-col lg:flex-row gap-5">
+        {/*------------stat------1-------*/}
+        <div className="rounded-lg bg-base-100 w-full shadow-xl text-white py-6 px-6">
+          <div className="flex items-center  justify-center gap-4">
+            <FaDollarSign className="text-4xl" />
+            <div>
+              {/* <div className="stat-value">{stats.revenue}</div> */}
+              <div className="text-2xl">Revenue</div>
+            </div>
+          </div>
+        </div>
+        {/*------------stat------2-------*/}
+        <div className="rounded-lg bg-base-100 w-full shadow-xl text-white py-6 px-6">
+          <div className="flex items-center  justify-center gap-4">
+            <FaUsers className="text-4xl" />
+            <div>
+              {/* <div className="stat-value">{stats.users}</div> */}
+              <div className="text-2xl">Users</div>
+            </div>
+          </div>
+        </div>
+        {/*------------stat------3-------*/}
+        <div className="rounded-lg bg-base-100 w-full shadow-xl text-white py-6 px-6">
+          <div className="flex items-center justify-center gap-4">
+            <GiAmpleDress className="text-4xl" />
+            <div>
+              {/* <div className="stat-value">{stats.menuItems}</div> */}
+              <div className="text-2xl">Users</div>
+            </div>
+          </div>
+        </div>
+        {/*------------stat------4-------*/}
+        <div className="rounded-lg bg-base-100 w-full shadow-xl text-white py-6 px-6">
+          <div className="flex items-center justify-center gap-4">
+            <FaFirstOrder className="text-4xl" />
+            <div>
+              {/* <div className="stat-value">{stats.orders}</div> */}
+              <div className="text-2xl">Orders</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
